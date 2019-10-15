01fc68a6bda075ad6b7a9af55e4e5eb278790eff
volerdman <37471065+volerdman@users.noreply.github.com>
2019-10-15 14:33:44 +0400

    десериализация

---
+++ SomeForm/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormTestProject
+{
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public String Name { get; set; }
+        [DataMember]
+        public String Surname { get; set; }
+        [DataMember]
+        public String FatherName { get; set; }
+
+        public Student(String name, String surname, String fatherName)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.FatherName = fatherName;
+        }
+    }
+}
--- StudentAccounting/StudentControlAccounting/userNonVisualControlDmitriev.cs
+++ StudentAccounting/StudentControlAccounting/userNonVisualControlDmitriev.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +22,15 @@ namespace StudentControlAccounting
             container.Add(this);
 
             InitializeComponent();
+        }
+
+        public static Student[] Deserialize(string file)
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Student[]));
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            {
+                return (Student[])jsonFormatter.ReadObject(fs);
+            }
         }
     }
 }
