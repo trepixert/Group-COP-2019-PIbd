a3f9919b531f151eb98a3ea2db277356c86cf353
volerdman <37471065+volerdman@users.noreply.github.com>
2019-10-15 14:17:03 +0400

    2 лаба

---
+++ StudentAccounting/StudentControlAccounting/userNonVisualControlDmitriev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentControlAccounting
+{
+    public partial class userNonVisualControlDmitriev : Component
+    {
+        public userNonVisualControlDmitriev()
+        {
+            InitializeComponent();
+        }
+
+        public userNonVisualControlDmitriev(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
